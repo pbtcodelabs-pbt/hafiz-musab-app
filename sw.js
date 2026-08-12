afd54b70ec29722ecfea3d5d721ea9345fc309f4
pbtcodelabs-pbt <pbtcodelabs@gmail.com>
2026-08-13 02:19:02 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// 1308th0209am — Service Worker
-const CACHE_NAME = 'hafiz-musab-shell-1308th0209am';
+// 1308th0216am — Service Worker
+const CACHE_NAME = 'hafiz-musab-shell-1308th0216am';
 const SHELL_FILES = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES).catch(() => {})));
